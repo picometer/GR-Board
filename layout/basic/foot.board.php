24d71470474db689600a564462c7d01670c5aab1
sirini <sirini@gmail.com>
2012-01-24 16:26:04 +0900

    삭제된 파일들을 버젼관리에서 제외함.

--- layout/basic/foot.board.php
+++
@@ -1,9 +0,0 @@
-</div>
-<div class="clear"></div>
-
-<div id="bottomFrame">
-Copyright ⓒ 2008 <?php echo $_SERVER['HTTP_HOST']; ?>. All rights reserved.
-</div>
-
-</body>
-</html>
--- layout/basic/foot.page.php
+++
@@ -1,6 +0,0 @@
-<div id="bottomFrame">
-Copyright ⓒ 2008 <?php echo $_SERVER['HTTP_HOST']; ?>. All rights reserved.
-</div>
-
-</body>
-</html>
